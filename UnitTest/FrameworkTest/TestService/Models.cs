ab9d009cde1a7417c76a5948a3552920b9bf77d8
阿布天 <renbuqi@163.com>
2015-10-26 15:57:59 +0800

    初始化整个文件夹

---
+++ UnitTest/FrameworkTest/TestService/Models.cs
@@ -0,0 +1,9 @@
+﻿namespace CCNUnitTests.FrameworkTest.TestService
+{
+    public class ReturnModel
+    {
+        public int code { get; set; }
+
+        public object content { get; set; }
+    }
+}
